782204fb7d92235ca1ad22b4c0f085bce6937706
Hayden <maghsk2017@gmail.com>
2022-11-30 13:50:39 +0800

    user agent

--- 1-1-1.urlCrawler/webgl/webgl/settings.py
+++ 1-1-1.urlCrawler/webgl/webgl/settings.py
@@ -23,6 +23,8 @@ KEYWORDS = [
    '<canvas',
 ]
 
+USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
+
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'webgl (+http://www.yourdomain.com)'
 
