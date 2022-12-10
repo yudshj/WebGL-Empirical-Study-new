e349ab91e41be2f66b61307fd985824312772c48
Hayden <maghsk2017@gmail.com>
2022-12-10 23:20:38 +0800

    fix bug

--- 1-1-1.urlCrawler/run.sh
+++ 1-1-1.urlCrawler/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd webgl
-echo "scrapy crawl tranco -s TRANCO_BEGIN=$1 -s TRANCO_END=$2"
+echo "scrapy crawl tranco -s TRANCO_BEGIN=$1 -s TRANCO_END=$2 -s LOG_FILE=/storage/webgl/output/scrapy-info_$1-$2.log"
 $HOME/.local/bin/scrapy crawl tranco -s TRANCO_BEGIN=$1 -s TRANCO_END=$2 -s LOG_FILE=/storage/webgl/output/scrapy-info_$1-$2.log
