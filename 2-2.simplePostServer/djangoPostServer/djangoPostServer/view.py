94a031e65bfe2cea03fe871dd360bd1e7c1cef4e
Yudong Han <maghsk2017@gmail.com>
2022-12-16 23:54:27 +0800

    use post http server to store json

---
+++ 2-2.simplePostServer/djangoPostServer/djangoPostServer/view.py
@@ -0,0 +1,21 @@
+import io
+
+from django.http import HttpResponse, HttpRequest
+import json
+import gzip
+
+def post(request: HttpRequest):
+    if request.method == 'POST':
+        yudong: str = request.headers.get("Yudong")
+        yudong = yudong.split("/")[2]
+        # with io.BytesIO() as buffer:
+        #     for chunk in request.FILES['data'].chunks():
+        #         buffer.write(chunk)
+        #     body = buffer.getvalue()
+        #     dumped_json = json.loads(gzip.decompress(body))
+        with open(f"../output/{yudong}.json.gz", "wb") as fp:
+            for chunk in request.FILES['data'].chunks():
+                fp.write(chunk)
+        return HttpResponse(json.dumps({'status': 'ok'}))
+    else:
+        return HttpResponse(json.dumps({'status': 'method error'}))
