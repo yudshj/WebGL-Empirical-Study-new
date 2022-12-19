06593dce44c7388568d47bdbf6f4c45fd1cae94e
Yudong Han <maghsk2017@gmail.com>
2022-12-19 19:21:47 +0800

    OJ8K!

--- 2-2.simplePostServer/djangoPostServer/djangoPostServer/view.py
+++ 2-2.simplePostServer/djangoPostServer/djangoPostServer/view.py
@@ -4,17 +4,28 @@ from django.http import HttpResponse, HttpRequest
 import json
 import gzip
 
+# def post(request: HttpRequest):
+#     if request.method == 'POST':
+#         yudong: str = request.headers.get("Yudong")
+#         # with io.BytesIO() as buffer:
+#         #     for chunk in request.FILES['data'].chunks():
+#         #         buffer.write(chunk)
+#         #     body = buffer.getvalue()
+#         #     dumped_json = json.loads(gzip.decompress(body))
+#         with open(f"../output/{yudong}.json.gz", "wb") as fp:
+#             for chunk in request.FILES['data'].chunks():
+#                 fp.write(chunk)
+#         return HttpResponse(json.dumps({'status': 'ok'}))
+#     else:
+#         return HttpResponse(json.dumps({'status': 'method error'}))
+
+
 def post(request: HttpRequest):
     if request.method == 'POST':
+        print(len(request.body), request.headers)
         yudong: str = request.headers.get("Yudong")
-        # with io.BytesIO() as buffer:
-        #     for chunk in request.FILES['data'].chunks():
-        #         buffer.write(chunk)
-        #     body = buffer.getvalue()
-        #     dumped_json = json.loads(gzip.decompress(body))
-        with open(f"../output/{yudong}.json.gz", "wb") as fp:
-            for chunk in request.FILES['data'].chunks():
-                fp.write(chunk)
+        with gzip.open(f"../output/{yudong}.json.gz", "wb") as fp:
+            fp.write(request.body)
         return HttpResponse(json.dumps({'status': 'ok'}))
     else:
         return HttpResponse(json.dumps({'status': 'method error'}))
