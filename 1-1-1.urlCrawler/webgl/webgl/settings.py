ef45f82a7edc6c9a14d4a62981523e509c9ca86b
Hydrogen <maghsk2017@gmail.com>
2023-02-06 12:17:19 +0800

    Update settings.py

--- 1-1-1.urlCrawler/webgl/webgl/settings.py
+++ 1-1-1.urlCrawler/webgl/webgl/settings.py
@@ -21,6 +21,8 @@ KEYWORDS = [
    'getContext',
    'getContext("webgl',
    '<canvas',
+   'clearColor',
+   'useProgram',
 ]
 
 USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
