6145c81b00e563916bc1e8e6828abf530f1c7d61
Yudong <maghsk2017@gmail.com>
2023-04-19 00:09:50 +0800

    show_images

--- 2-6.label/show_images.py
+++ 2-6.label/show_images.py
@@ -23,24 +23,17 @@ HELP = '''
 '1' for 0, 1, 1, ''
 '2' for 0, 2, 1, ''
 '3' for 0, 3, 0, ''
-'d' for 0, 3, 0, 'domain'
 '4' for 0, 4, 0, ''
 'e' for 1, None, None, None
+'c' for 2, None, None, None
+'b' for 3, None, None, None
+'d' for 4, None, None, 'domain'
 't' for None, None, None, 'TODO'
 '''
 
 def main():
-    # fp = open('output.csv', 'a')
-    # show image one by one
-    # fig, ax = plt.subplots()
     df = pd.DataFrame(columns=['error', 'use_case', 'need_interactions', 'comments'])
-    # for i in range(START, END+1):
-    #     p = IMG_PATH / f'{i:05d}.png'
-    #     if p.exists():
-    #         df.loc[i] = [None, None, None, 'TODO']
-    #     else:
-    #         df.loc[i] = [None, None, None, 'File not found']
-    # set dtype
+
     df['error'] = df['error'].astype('Int64')
     df['use_case'] = df['use_case'].astype('Int64')
     df['need_interactions'] = df['need_interactions'].astype('Int64')
@@ -81,14 +74,20 @@ def main():
                 elif key == ord('3'):
                     df.loc[i] = [0, 3, 0, '']
                     break
-                elif key == ord('d'):
-                    df.loc[i] = [0, 3, 0, 'domain']
-                    break
                 elif key == ord('4'):
                     df.loc[i] = [0, 4, 0, '']
                     break
                 elif key == ord('e'):
                     df.loc[i] = [1, None, None, None]
+                    break
+                elif key == ord('c'):
+                    df.loc[i] = [2, None, None, None]
+                    break
+                elif key == ord('b'):
+                    df.loc[i] = [3, None, None, None]
+                    break
+                elif key == ord('d'):
+                    df.loc[i] = [4, None, None, 'domain']
                     break
                 elif key == ord('t'):
                     df.loc[i] = [None, None, None, 'TODO']
