ceb628ba4fbe02223baf15b2854d20e61f8dbb5c
Yudong <maghsk2017@gmail.com>
2023-04-21 13:07:06 +0800

    0420

--- 2-6.label/show_images.py
+++ 2-6.label/show_images.py
@@ -2,13 +2,10 @@ from pathlib import Path
 import pandas as pd
 import numpy as np
 import datetime
-# import matplotlib.pyplot as plt
-# import matplotlib
 import cv2
 
 
 
-# output/2022-01-01
 START = 0
 END = 1578
 IMG_PATH = Path('input/all/screenshot')
@@ -16,20 +13,29 @@ OUT_PATH = Path('output') / (datetime.datetime.now().strftime('%Y-%m-%d') + f'_{
 
 OUT_PATH.parent.mkdir(exist_ok=True, parents=True)
 
+COLUMNS = ['key', 'error', '用况', '交互', 'comments']
 
-HELP = '''
-'0' for 0, 1, 0, ''
-'s' for 0, 1, 0, 'scroll'
-'1' for 0, 1, 1, ''
-'2' for 0, 2, 1, ''
-'3' for 0, 3, 0, ''
-'4' for 0, 4, 0, ''
-'e' for 1, None, None, None
-'c' for 2, None, None, None
-'b' for 3, None, None, None
-'d' for 4, None, None, 'domain'
-'t' for None, None, None, 'TODO'
-'''
+DICT = {
+    ord('0'): [0, 1, 0, ''],
+    ord('s'): [0, 1, 0, 'scroll'],
+    ord('1'): [0, 1, 1, ''],
+    ord('2'): [0, 2, 1, ''],
+    ord('3'): [0, 3, 0, ''],
+    ord('4'): [0, 4, 0, ''],
+    ord('e'): [1, None, None, None],
+    ord('c'): [2, None, None, None],
+    ord('b'): [3, None, None, None],
+    ord('d'): [4, None, None, 'domain'],
+    ord('w'): [0, 1, 1, 'waiting'],
+    ord('t'): [None, None, None, 'TODO'],
+}
+
+def print_help():
+    print('<error> := 0: no error, 1: http error, 2: cloudflare 验证, 3: 白屏/黑屏, 4: 域名错误')
+    print('<用况> := 1: 渲染, 2: 加速计算, 3: 未知, 4: 追踪')
+    print('\t'.join(COLUMNS))
+    for k,v in DICT.items():
+        print(chr(k) + '\t' + '\t'.join([str(x) for x in v]))
 
 def main():
     df = pd.DataFrame(columns=['error', 'use_case', 'need_interactions', 'comments'])
@@ -52,45 +58,15 @@ def main():
             # plt.show(block=False)
             while True:
                 # print(lst)
-                print(HELP)
+                print_help()
                 print("INDEX:", i)
                 key = cv2.waitKey(0)
                 try:
                     print('Key pressed:', chr(key))
                 except:
                     print('Key pressed #hex:', key)
-                if key == ord('0'):
-                    df.loc[i] = [0, 1, 0, '']
-                    break
-                elif key == ord('s'):
-                    df.loc[i] = [0, 1, 0, 'scroll']
-                    break
-                elif key == ord('1'):
-                    df.loc[i] = [0, 1, 1, '']
-                    break
-                elif key == ord('2'):
-                    df.loc[i] = [0, 2, 1, '']
-                    break
-                elif key == ord('3'):
-                    df.loc[i] = [0, 3, 0, '']
-                    break
-                elif key == ord('4'):
-                    df.loc[i] = [0, 4, 0, '']
-                    break
-                elif key == ord('e'):
-                    df.loc[i] = [1, None, None, None]
-                    break
-                elif key == ord('c'):
-                    df.loc[i] = [2, None, None, None]
-                    break
-                elif key == ord('b'):
-                    df.loc[i] = [3, None, None, None]
-                    break
-                elif key == ord('d'):
-                    df.loc[i] = [4, None, None, 'domain']
-                    break
-                elif key == ord('t'):
-                    df.loc[i] = [None, None, None, 'TODO']
+                if key in DICT:
+                    df.loc[i] = DICT[key]
                     break
                 elif key == ord('z'):
                     if len(lst) == 0:
