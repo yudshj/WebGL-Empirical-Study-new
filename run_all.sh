81684dd28c6f7680fd6fdd7390e9749d3c927240
Yudong <maghsk2017@gmail.com>
2023-04-23 23:53:57 +0800

    run all !!!

---
+++ run_all.sh
@@ -0,0 +1,6 @@
+cd /home/hanyd/Code/Python/WES/2-3.seleniumCrawler/playwright/
+./run_simple.sh
+cd /home/hanyd/Code/Python/WES/2-3.seleniumCrawler/playwright/
+./run_all.sh
+cd /home/hanyd/Code/Python/WES/2-7.webglCapture/playwright/
+./run_all.sh
