9338e7c8aaf3e19eb84b5b147dae8040a85e481d
Yudong <maghsk2017@gmail.com>
2023-04-24 12:29:36 +0800

    不多不少，正好12个！

--- run_all.sh
+++ run_all.sh
@@ -1,6 +1,10 @@
+cd /home/hanyd/Code/Python/WebGL-vs-WebGPU/django-server
+tmux new -d -s django python3 manage.py runserver
 cd /home/hanyd/Code/Python/WES/2-3.seleniumCrawler/playwright/
 ./run_simple.sh
+sleep 5
 cd /home/hanyd/Code/Python/WES/2-3.seleniumCrawler/playwright/
 ./run_all.sh
+sleep 5
 cd /home/hanyd/Code/Python/WES/2-7.webglCapture/playwright/
 ./run_all.sh
