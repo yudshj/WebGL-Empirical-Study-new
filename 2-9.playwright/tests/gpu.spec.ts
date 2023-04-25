a499b8c5511993b871c26fb6a129b5d7646f9402
Yudong Han <maghsk2017@gmail.com>
2023-04-25 21:46:06 +0800

    gpu test

--- 2-9.playwright/tests/gpu.spec.ts
+++ 2-9.playwright/tests/gpu.spec.ts
@@ -1,19 +1,32 @@
-import { test, expect } from '@playwright/test';
-
-test.use({
-  headless: true,
-  // viewport: { width: 1280, height: 720 },
-  ignoreHTTPSErrors: true,
-  launchOptions: {
-    args: [
-      "--use-gl=egl",
-      "--enable-gpu",
-    ]
-  }
+// tests/example1.test.js
+const { test, expect } = require('@playwright/test');
+function waitFor(delay) {
+    return new Promise(resolve => setTimeout(resolve, delay));
+}
+const currentTime = Date.now();
+const url1 = "chrome://gpu/";
+const url2 = "https://www.soft8soft.com/webglreport";
+const url3 = "https://webglsamples.org/aquarium/aquarium.html";
+test.beforeEach(async ({ page }, testInfo) => {
+    testInfo.setTimeout(testInfo.timeout + 160000);
 });
-
-test("GPU hardware acceleration", async ({ page }) => {
-  await page.goto("chrome://gpu")
-  let featureStatusList = page.locator(".feature-status-list")
-  await expect(featureStatusList).toContainText("Hardware accelerated")
-});
+test.describe('Testing 123', () => {
+    // Check if hardware acceleration is enabled. Without it, our tests will be much slower.
+    test("1. GPU hardware acceleration", async ({ page }) => {
+        await page.goto(url1);
+        let featureStatusList = page.locator(".feature-status-list")
+        await waitFor(2000);
+        await page.screenshot({ path: 'screens/screenshot' + currentTime + '_1.png', fullPage: true });
+        // await expect(featureStatusList).toContainText("Hardware accelerated")
+    })
+    test("2. webgl report", async ({ page }) => {
+        await page.goto(url2)
+        await waitFor(2000);
+        await page.screenshot({ path: 'screens/screenshot' + currentTime + '_2.png', fullPage: true });
+    })
+    test("3. aquarium", async ({ page }) => {
+        await page.goto(url3)
+        await waitFor(5000);
+        await page.screenshot({ path: 'screens/screenshot' + currentTime + '_3.png', fullPage: true });
+    })
+})
