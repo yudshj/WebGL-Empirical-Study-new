6eb8bdd60ca696e02842790c95d7c8c5530c6a49
Yudong Han <maghsk2017@gmail.com>
2023-04-26 12:31:04 +0800

    add timeout to Promise.allsettled

--- 2-9.playwright/tasks/cap.ts
+++ 2-9.playwright/tasks/cap.ts
@@ -3,7 +3,7 @@ import * as fs from 'fs';
 import * as zlib from "zlib";
 
 
-const idx_url: string[][] = JSON.parse(fs.readFileSync('input/0422.json', 'utf8'));
+// const idx_url: string[][] = JSON.parse(fs.readFileSync('input/0422.json', 'utf8'));
 // const idx_url = [
 //   ["TEST-0", "https://www.babylonjs.com/demos/sponza/"],
 //   ["TEST-1", "https://www.pola.co.jp/special/o/wecaremore/mothersday/"],
@@ -14,6 +14,16 @@ const idx_url: string[][] = JSON.parse(fs.readFileSync('input/0422.json', 'utf8'
 //   ["TEST-6", "https://vrseat.vercel.app/"],
 //   ["TEST-7", "http://campoallecomete.it/"],
 // ];
+const idx_url = [
+  ["BUG-0", "https://pt.gamcore.com/jogos/brothel_empire_unity_version"],
+  ["BUG-1", "https://www.bingoaloha.com/"],
+  ["BUG-2", "https://www.miuraz.com.tw/"],
+  ["BUG-3", "https://automotive.products.constellium.com/"],
+  ["BUG-4", "https://app.austrianskigame.com/game/"],
+  ["BUG-5", "http://gym-drimou.thess.sch.gr/"],
+  ["BUG-6", "https://www.ggpschas.org/"],
+  ["BUG-7", "https://www.vocedivulga.com/carrega-troca-links.php?id=782&_xafvr=ZTNkMWE2MjlhMThjNzQ2M2JlYjgyMDk5OTBhMGU3YTE1Y2ZhMDE1ZCw2NDQ4OWE1YjEzODBk"],
+];
 const total = idx_url.length;
 
 const PART = parseInt(process.argv[2]);
@@ -22,39 +32,48 @@ const PART_SIZE = Math.ceil(total / TOTAL_PART);
 const START = PART * PART_SIZE;
 const END = Math.min((PART + 1) * PART_SIZE, total);
 
-console.log(START, "to", END)
+console.info(START, "to", END)
 
 fs.mkdirSync('output/cap/', { recursive: true });
 
-const evaluate_script_in_all_frames = (page: Page, str: string) => {
-  return Promise.allSettled(page.frames().map((frame: Frame) => frame.evaluate(str).catch(() => null)));
+const rejectOnTimeout = (delay: number) => new Promise((_, reject) => {
+  setTimeout(reject, delay, new Error("rejectOnTimeout"));
+});
+
+
+const evaluate_script_in_all_frames = (page: Page, str: string, timeout: number) => {
+  console.info(`* evaluate_script_in_all_frames(${str});`);
+  return Promise.allSettled(page.frames().map((frame: Frame) => Promise.race([frame.evaluate(str), rejectOnTimeout(timeout)]).catch(() => null)));
 }
 
-const wait_for_function_in_all_frames = (page: Page, str: string) => {
-  return Promise.allSettled(page.frames().map((frame: Frame) => frame.waitForFunction(str).catch(() => null)));
+const wait_for_function_in_all_frames = (page: Page, str: string, timeout: number) => {
+  console.info(`* wait_for_function_in_all_frames(${str});`);
+  return Promise.allSettled(page.frames().map((frame: Frame) => Promise.race([frame.waitForFunction(str), rejectOnTimeout(timeout)]).catch(() => null)));
 }
 
-const get_data_in_all_frames = (page: Page, str: string) => {
-  return Promise.all(
-    page.frames().map(async (frame: Frame) => {
-      try {
-        const data = await frame.evaluate(str);
+const get_data_in_all_frames = (page: Page, str: string, timeout: number) => {
+  console.info(`* GET_DATA_IN_ALL_FRAMES(${str});`);
+  const promises = page.frames().map((frame: Frame) => {
+    return Promise.race([frame.evaluate(str), rejectOnTimeout(timeout)])
+      .then((data) => {
         return {
           name: frame.name(),
           url: frame.url(),
           data: data,
           error: null,
         };
-      } catch (error) {
+      })
+      .catch((error) => {
         return {
           name: frame.name(),
           url: frame.url(),
-          data: null, // 或者您可以设置一个默认值
+          data: null,
           error: error,
         };
-      }
-    })
-  );
+      });
+  });
+
+  return Promise.allSettled(promises);
 }
 
 (async () => {
@@ -80,6 +99,7 @@ const get_data_in_all_frames = (page: Page, str: string) => {
         headless: false,
       });
       try {
+        console.info('launch browser')
         const context = await browser.newContext({ ignoreHTTPSErrors: true });
         await context.addInitScript({ path: 'js/inject-tiny.js' });
         await context.addInitScript({ path: 'js/webgl-capture.js' });
@@ -88,23 +108,27 @@ const get_data_in_all_frames = (page: Page, str: string) => {
         const date = Date.now();
         const start_time_hp = performance.now();
 
-        await page.goto(url, { waitUntil: 'networkidle', timeout: 60_000 })
-          .then(() => evaluate_script_in_all_frames(page, "HydWebGLCapture.debugInfoAll('net_idle - OK');"))
-          .catch(() => evaluate_script_in_all_frames(page, "HydWebGLCapture.debugInfoAll('net_idle - ERROR (TIMEOUT?)');"))
+        console.info('goto');
+        await page.goto(url, { waitUntil: 'networkidle', timeout: 30_000 })
+          .then(() => evaluate_script_in_all_frames(page, "HydWebGLCapture.debugInfoAll('net_idle - OK');", 10_000))
+          .catch(() => evaluate_script_in_all_frames(page, "HydWebGLCapture.debugInfoAll('net_idle - ERROR (TIMEOUT?)');", 10_000))
           .catch(() => null);
-
+        
+        console.info('net idle');
         const net_idle_time_hp = performance.now();
-        await evaluate_script_in_all_frames(page, "HydWebGLCapture.debugInfoAll('net_idle');");
-        const net_idle_counters = await get_data_in_all_frames(page, "window.hydGetCounters();");
+        const net_idle_counters = await get_data_in_all_frames(page, "window.hydGetCounters();", 10_000);
 
         await page.waitForTimeout(15_000);
-        await evaluate_script_in_all_frames(page, "HydWebGLCapture.stopAll();");
-        await wait_for_function_in_all_frames(page, "HydWebGLCapture.allStopped();");
 
+        console.info('stop')
+        await evaluate_script_in_all_frames(page, "HydWebGLCapture.stopAll();", 10_000);
+        await wait_for_function_in_all_frames(page, "HydWebGLCapture.allStopped();", 10_000);
+        
+        console.info('capture');
         const gl_cap_time_hp = performance.now();
-        await evaluate_script_in_all_frames(page, "HydWebGLCapture.debugInfoAll('gl_cap');");
-        const gl_cap_counters = await get_data_in_all_frames(page, "window.hydGetCounters();");
-        const gl_captures = await get_data_in_all_frames(page, "HydWebGLCapture.generateAll();");
+        await evaluate_script_in_all_frames(page, "HydWebGLCapture.debugInfoAll('gl_cap');", 10_000);
+        const gl_cap_counters = await get_data_in_all_frames(page, "window.hydGetCounters();", 10_000);
+        const gl_captures = await get_data_in_all_frames(page, "HydWebGLCapture.generateAll();", 30_000);
 
         const data = {
           url,
