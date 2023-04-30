3a3b08ea7903599e8e3b5bba0c1c939a9e211537
Yudong <maghsk2017@gmail.com>
2023-05-01 07:50:43 +0800

    0430

--- 2-9.playwright/tasks/spector.ts
+++ 2-9.playwright/tasks/spector.ts
@@ -3,6 +3,9 @@ import * as fs from 'fs';
 import { evaluate_script_in_all_frames, wait_for_function_in_all_frames, get_data_in_all_frames } from './utils/utils';
 import { indexUrls } from './utils/config';
 
+const NAME = 'cap';
+fs.mkdirSync(`output/${NAME}/`, { recursive: true });
+
 (async () => {
     const browser = await chromium.launch({
         headless: false,
@@ -21,18 +24,14 @@ import { indexUrls } from './utils/config';
     try {
         const page = await browserContext.newPage();
         await page.goto('https://webglsamples.org/aquarium/aquarium.html', { timeout: 30_000, waitUntil: 'networkidle' });
-        await page.waitForTimeout(1000);
+        await page.waitForTimeout(1_000);
         await evaluate_script_in_all_frames(page, 'hydSpectorStart()', 10_000);
-        await page.waitForTimeout(10000);
-
-        console.log(`waitForFunction`);
+        await page.waitForTimeout(10_000);
         await wait_for_function_in_all_frames(page, 'window._hydCaptured.length === window._hydSpectors.length', 10_000);
-
-        console.log(`const cap = await page.evaluate('window._hydCaptured')`);
-        // const cap = await page.evaluate('window._hydCaptured');
         const cap = await get_data_in_all_frames(page, 'window._hydCaptured', 30_000);
-        fs.mkdirSync('output', { recursive: true });
-        fs.writeFileSync('output/capture.json', JSON.stringify(cap, null, 2));
+        fs.writeFileSync('output/spector/capture.json', JSON.stringify(cap, null, 2));
+
+        // await page.waitForTimeout(3600_000);
     }
     catch (e) {
         console.error(e);
