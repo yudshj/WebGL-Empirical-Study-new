e9167765f26bdb6cc85db2a0782284401c058d5b
Yudong Han <maghsk2017@gmail.com>
2023-05-01 17:34:36 +0800

    run_one_task.sh

--- 2-9.playwright/run_all.sh
+++ 2-9.playwright/run_all.sh
@@ -1,11 +1,9 @@
 echo $@
 echo "..."
 
+N=10
+
 for NAME in $@
 do
-    tmux new -s "${NAME}-1_4" -d ts-node tasks/${NAME}.ts 0 4
-    tmux new -s "${NAME}-2_4" -d ts-node tasks/${NAME}.ts 1 4
-    tmux new -s "${NAME}-3_4" -d ts-node tasks/${NAME}.ts 2 4
-    tmux new -s "${NAME}-4_4" -d ts-node tasks/${NAME}.ts 3 4
-    sleep 5
+    ./run_one_task.sh $NAME $N
 done
---
+++ 2-9.playwright/run_one_task.sh
@@ -0,0 +1,8 @@
+name=$1
+N=$2
+
+for i in $(seq 0 $((N-1))); do
+    echo tmux new -s "${name}-${i}_${N}" -d ts-node tasks/"${name}".ts ${i} ${N}
+    tmux new -s "${name}-${i}_${N}" -d ts-node tasks/"${name}".ts ${i} ${N}
+    sleep 5
+done
