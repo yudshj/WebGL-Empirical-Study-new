4922283047007d3d99e9593aaa1b58bbba7bbd30
Yudong <maghsk2017@gmail.com>
2023-05-02 17:51:34 +0800

    bug fix

--- 2-9.playwright/js/inject-gl.js
+++ 2-9.playwright/js/inject-gl.js
@@ -152,7 +152,7 @@ function HydNewGetContext() {
                                 || name.toLowerCase().indexOf('texsub') !== -1
                                 || name.toLowerCase().indexOf('texstorage') !== -1) {
                             for (const arg of arguments) {
-                                if (typeof arg === 'object') {
+                                if (arg !== null && arg.constructor !== null && name in arg.constructor && typeof arg === 'object') {
                                     context.maghsk.counter.textureTypes[arg.constructor.name] = (context.maghsk.counter.textureTypes[arg.constructor.name] || 0) + 1;
                                     if (   arg instanceof ImageData
                                         || arg instanceof HTMLImageElement
