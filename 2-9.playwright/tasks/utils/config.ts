a6e246926de713aca721c7080cc9dd187beca235
Yudong Han <maghsk2017@gmail.com>
2023-06-02 21:11:57 +0800

    okkok

--- 2-9.playwright/tasks/utils/config.ts
+++ 2-9.playwright/tasks/utils/config.ts
@@ -8,28 +8,28 @@ interface LaunchOptionsDict {
 const useProxy = true;
 const headless = false;
 
-export const indexUrls: string[][] = JSON.parse(fs.readFileSync('input/0422.json', 'utf8'));
+// export const indexUrls: string[][] = JSON.parse(fs.readFileSync('input/0422.json', 'utf8'));
 // export const indexUrls: string[][] = JSON.parse(fs.readFileSync('input/0512-df_label-raf-need_interactions.json', 'utf8'));
-// export const indexUrls = [
-//   ["TEST-0", "https://www.babylonjs.com/demos/sponza/"],
-//   ["TEST-1", "https://www.pola.co.jp/special/o/wecaremore/mothersday/"],
-//   ["TEST-2", "https://domenicobrz.github.io/webgl/projects/SSRefractionDepthPeeling/"],
-//   ["TEST-3", "https://domenicobrz.github.io/webgl/projects/experiment1/"],
-//   ["TEST-4", "https://needle.tools/"],
-//   ["TEST-5", "https://turbulent.ca/"],
-//   ["TEST-6", "https://vrseat.vercel.app/"],
-//   ["TEST-7", "http://campoallecomete.it/"],
-//   ["TEST-8", "http://donbeto.com/menu-restaurante/"],
-//   ["TEST-9", "https://gameofphones.rfl.dev"],
-//   ["TEST-10", "https://sandcastle.cesium.com/"], // webworker.
-//   ["TEST-11", "https://sandcastle.cesium.com/standalone.html#c=bY9Pa8MwDMW/ivGpgyGx6+qGQq+FHQY7+eI6Wmuq2MFSUtJPvyS9jK26vaf3059YsqgZE92omp3JdDMHkjR08LV6G2/jqg8la0iZqrcvW5/tq3WiE1Pjs1lrn7q+VDVD5Q0AKnU9ByXB0xCvpBBFFnCJOvyNujaNJrW7J5tM5CAyd74H5s90J28bh3P+H8oltCmfP0aqHKYldnlrjg8TABzO8jmppfAp1D+T5/8uqr28I0rIbQyiTPA4EGLp8ByYqU74Aw"], //Webworker
-//   ["TEST-12", "http://kripken.github.io/webgl-worker/playcanvas/simple-worker.html"],
-//   ["TEST-13", "https://sitnik.ru/en/"],
-//   ["TEST-14", "https://devnook.github.io/OffscreenCanvasDemo/keep-ui-responsive.html"], // offscreencanvas
-//   ["TEST-15", "https://devnook.github.io/OffscreenCanvasDemo/use-with-lib.html"], // offscreencanvas
-//   ["TEST-16", "https://threejs.org/examples/webgl_worker_offscreencanvas.html"], // offscreencanvas
-//   ["TEST-17", "https://codepen.io/Murugan1982/pen/WNojEXP"], // offscreencanvas
-// ];
+export const indexUrls = [
+  ["TEST-0", "https://www.babylonjs.com/demos/sponza/"],
+  ["TEST-1", "https://www.pola.co.jp/special/o/wecaremore/mothersday/"],
+  ["TEST-2", "https://domenicobrz.github.io/webgl/projects/SSRefractionDepthPeeling/"],
+  ["TEST-3", "https://domenicobrz.github.io/webgl/projects/experiment1/"],
+  ["TEST-4", "https://needle.tools/"],
+  ["TEST-5", "https://turbulent.ca/"],
+  ["TEST-6", "https://vrseat.vercel.app/"],
+  ["TEST-7", "http://campoallecomete.it/"],
+  ["TEST-8", "http://donbeto.com/menu-restaurante/"],
+  ["TEST-9", "https://gameofphones.rfl.dev"],
+  ["TEST-10", "https://sandcastle.cesium.com/"], // webworker.
+  ["TEST-11", "https://sandcastle.cesium.com/standalone.html#c=bY9Pa8MwDMW/ivGpgyGx6+qGQq+FHQY7+eI6Wmuq2MFSUtJPvyS9jK26vaf3059YsqgZE92omp3JdDMHkjR08LV6G2/jqg8la0iZqrcvW5/tq3WiE1Pjs1lrn7q+VDVD5Q0AKnU9ByXB0xCvpBBFFnCJOvyNujaNJrW7J5tM5CAyd74H5s90J28bh3P+H8oltCmfP0aqHKYldnlrjg8TABzO8jmppfAp1D+T5/8uqr28I0rIbQyiTPA4EGLp8ByYqU74Aw"], //Webworker
+  ["TEST-12", "http://kripken.github.io/webgl-worker/playcanvas/simple-worker.html"],
+  ["TEST-13", "https://sitnik.ru/en/"],
+  ["TEST-14", "https://devnook.github.io/OffscreenCanvasDemo/keep-ui-responsive.html"], // offscreencanvas
+  ["TEST-15", "https://devnook.github.io/OffscreenCanvasDemo/use-with-lib.html"], // offscreencanvas
+  ["TEST-16", "https://threejs.org/examples/webgl_worker_offscreencanvas.html"], // offscreencanvas
+  ["TEST-17", "https://codepen.io/Murugan1982/pen/WNojEXP"], // offscreencanvas
+];
 // export const indexUrls = [
 //   ["HYD-0", "http://127.0.0.1:8000/test1/webgl/easy"],
 //   ["HYD-1", "http://127.0.0.1:8000/test2/webgl"],
@@ -61,6 +61,7 @@ export function getLaunchOptions(name: string): LaunchOptions {
       '--memory-pressure-off',
       '--max-old-space-size=8192',
       '--js-flags="--max_old_space_size=8192"',
+      '--ignore-certificate-errors',
     ],
   };
   if (useProxy) {
@@ -77,9 +78,7 @@ export function getLaunchOptions(name: string): LaunchOptions {
 const proxyPool = [
   // 'socks5://ss.maghsk.site:3539',
   // 'socks5://ss.maghsk.site:3535',
-  'socks5://162.105.175.55:10701',
-  'socks5://162.105.175.55:13838',
-  'socks5://162.105.175.55:13839',
+  'socks5://127.0.0.1:7890',
 ];
 
 export const contextOptions: BrowserContextOptions = { locale: 'en-US', ignoreHTTPSErrors: true, permissions: ['camera', 'microphone'] };
