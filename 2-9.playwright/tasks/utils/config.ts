607db0a3330dfa3908fe2277865b1e26691d213c
Yudong Han <maghsk2017@gmail.com>
2023-06-17 15:43:44 +0800

    bugs https://www.stickittothestickman.com/

--- 2-9.playwright/tasks/utils/config.ts
+++ 2-9.playwright/tasks/utils/config.ts
@@ -1,5 +1,6 @@
 import * as fs from 'fs';
 import { LaunchOptions, BrowserContextOptions } from 'playwright';
+import { Protocol } from 'playwright-core/types/protocol';
 
 interface LaunchOptionsDict {
   [key: string]: LaunchOptions;
@@ -8,7 +9,7 @@ interface LaunchOptionsDict {
 const useProxy = true;
 const headless = false;
 
-export const indexUrls: string[][] = JSON.parse(fs.readFileSync('input/0422.json', 'utf8'));
+// export const indexUrls: string[][] = JSON.parse(fs.readFileSync('input/0422.json', 'utf8'));
 // export const indexUrls: string[][] = JSON.parse(fs.readFileSync('input/0512-df_label-raf-need_interactions.json', 'utf8'));
 // export const indexUrls = [
 //   ["TEST-0", "https://www.babylonjs.com/demos/sponza/"],
@@ -36,18 +37,19 @@ export const indexUrls: string[][] = JSON.parse(fs.readFileSync('input/0422.json
 //   ["HYD-2", "http://127.0.0.1:8000/test3/webgl/easy"],
 //   ["HYD-3", "http://localhost:8000/"],
 // ];
-// export const indexUrls = [
-//   ["BUG-0", "http://12storytellers.hands.agency"],
-//   ["BUG-1", "http://12vendome.chaumet.com/"],
-//   ["BUG-2", "http://360.jomsborg-vineta.com/"],
-//   ["BUG-3", "http://accountsavvycpa.com"],
-//   ["BUG-4", "https://code.irobot.com"],
-//   ["BUG-5", "http://www.kleuterspel.be/"],
-//   ["BUG-6", "https://3n1gm4.space/"],
-//   ["BUG-7", "http://www.acsce.edu.in/"],
-//   ["BUG-8", "https://eikanya.github.io/"],
-//   ["BUG-9", "https://code.irobot.com/"],
-// ];
+export const indexUrls = [
+  ["BUG-0", "http://12storytellers.hands.agency"],
+  ["BUG-1", "http://12vendome.chaumet.com/"],
+  ["BUG-2", "http://360.jomsborg-vineta.com/"],
+  ["BUG-3", "http://accountsavvycpa.com"],
+  ["BUG-4", "https://code.irobot.com"],
+  ["BUG-5", "http://www.kleuterspel.be/"],
+  ["BUG-6", "https://3n1gm4.space/"],
+  ["BUG-7", "http://www.acsce.edu.in/"],
+  ["BUG-8", "https://eikanya.github.io/"],
+  ["BUG-9", "https://code.irobot.com/"],
+  ["BUG-10", "https://www.stickittothestickman.com/"],
+];
 
 const launchOptions: LaunchOptionsDict = {};
 
@@ -84,3 +86,89 @@ const proxyPool = [
 ];
 
 export const contextOptions: BrowserContextOptions = { locale: 'en-US', ignoreHTTPSErrors: true, permissions: ['camera', 'microphone'] };
+
+export const myStartParameters: Protocol.Tracing.startParameters = {
+  transferMode: "ReturnAsStream",
+  streamCompression: "gzip",
+  streamFormat: "proto",
+  traceConfig: {
+      recordMode: "recordAsMuchAsPossible",
+      enableSampling: true,
+      enableSystrace: false,
+      traceBufferSizeInKb: 2097152,
+      memoryDumpConfig: {
+          // @ts-ignore
+          allowed_dump_modes: [
+              "background",
+              "light",
+              "detailed"
+          ],
+          // @ts-ignore
+          triggers: [
+              {
+                  "min_time_between_dumps_ms": 500,
+                  "mode": "detailed",
+                  "type": "periodic_interval"
+              }
+          ]
+      },
+      includedCategories: [
+          "base",
+          "benchmark",
+          "blink",
+          "browser",
+          "cc",
+          "cpu_profiler",
+          "disabled-by-default-cpu_profiler.debug",
+          "disabled-by-default-cpu_profiler",
+          "disabled-by-default-gpu_cmd_queue",
+          "disabled-by-default-gpu.dawn",
+          "disabled-by-default-gpu.debug",
+          "disabled-by-default-gpu.decoder",
+          "disabled-by-default-gpu.device",
+          "disabled-by-default-gpu.service",
+          "disabled-by-default-ipc.flow",
+          "disabled-by-default-memory-infra",
+          "disabled-by-default-memory-infra.v8.code_stats",
+          "disabled-by-default-mojom",
+          "disabled-by-default-toplevel.flow",
+          "disabled-by-default-toplevel.ipc",
+          "disabled-by-default-v8.cpu_profiler",
+          "gpu.angle",
+          "gpu",
+          "gpu.capture",
+          "ipc",
+          "loading",
+          "log",
+          "memory",
+          "mojom",
+          "navigation",
+          "native",
+          "net",
+          "netlog",
+          "scheduler",
+          "sequence_manager",
+          "toplevel.flow",
+          "toplevel",
+          "ui",
+          "v8.execute",
+          "v8.wasm",
+          "v8",
+          "views",
+          "viz",
+          "devtools.timeline",
+          "devtools.contrast",
+          "devtools",
+          "disabled-by-default-devtools.timeline.stack",
+          "disabled-by-default-devtools.timeline.picture",
+          "disabled-by-default-devtools.timeline.layers",
+          "disabled-by-default-devtools.timeline.invalidationTracking",
+          "disabled-by-default-devtools.timeline.inputs",
+          "disabled-by-default-devtools.timeline.frame",
+          "disabled-by-default-devtools.timeline"
+      ],
+      excludedCategories: [
+          "*"
+      ],
+  }
+};
